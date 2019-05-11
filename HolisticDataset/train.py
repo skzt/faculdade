55c93e4625aa4762be5a7269df4f9285423c89e9
skzt <pedroskzt@gmail.com>
2019-05-11 03:16:09 -0300

    Organization

--- HolisticDataset/train.py
+++ HolisticDataset/train.py
@@ -3,26 +3,29 @@ from time import strftime
 import numpy as np
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
-from lprnet import LPRNet
+# from lprnet import LPRNet
 
-# from HolisticDataset.lprnet import LPRNet
+from HolisticDataset.lprnet import LPRNet
 # Comentario pra vida: numpay array shape = (LINHAS, COLUNAS)
 
-IMG_SIZE = 64  # 64 x 64
-CLASSES_SIZE = 10  # 0 1 2 3 4 5 6 7 8 9
+IMG_SIZE = 28  # 64 x 64
+# ROW_SIZE = 9
+# COL_SIZE = 6
+CLASSES_SIZE = 2  # 0 1 2 3 4 5 6 7 8 9
 CLASSES = '0123456789'
 OUTPUTS = 1  # Número de saidas
 RANDOM_SEED = 8495
 
 LEARNING_RATE = 0.001
-EPOCHS = 1
+EPOCHS = 20
 BS = 32
 
-training_data = np.load("hlp-training_data-22800-Labeled-GrayScale-(64,9).npy")
+# training_data = np.load("alexandre-real-180-GrayScale-(9,6).npy")
+training_data = np.load("DogCat-25000-GrayScale-(28,28).npy")
 # 20% Para teste (4560)
 # 80% Para treino (18240)
-train = training_data[:-4560]
-test = training_data[-4560:]
+train = training_data[:-500]
+test = training_data[-500:]
 del training_data
 
 trainX = np.array([data[0] for data in train])
@@ -34,8 +37,8 @@ testY = np.array([data[1] for data in test])
 del train
 del test
 
-trainX = trainX.reshape(-1, IMG_SIZE, trainX[0].shape[1], 1)
-testX = testX.reshape(-1, IMG_SIZE, testX[0].shape[1], 1)
+trainX = trainX.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+testX = testX.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 trainX = trainX / 255.0
 testX = testX / 255.0
@@ -59,9 +62,9 @@ model.fit(trainX,
           validation_data=(testX, testY)
           )
 
-model.save(f"LR-LPR-("
+model.save(f"DogCat-("
            f"{trainX.shape[0] + testX.shape[0]}, {trainX.shape[1]}, {trainX.shape[2]}"
            f")-{LEARNING_RATE}"
-           f"-HLP-dataset-"
+           f"-alexandre-REAL-"
            f"{strftime('%d-%m-%Y')}"
            f".h5")
