b4d698a39ea5cd0e82ae43f0df30568376579563
Skzt-Desk <pedroskzt@gmail.com>
2019-06-19 17:31:39 -0300

    Atualização e organização da estrutura de pastas

--- AlexandreDataset/cv2Plate.py
+++ AlexandreDataset/cv2Plate.py
@@ -1,18 +1,20 @@
 # 1106200195976514
 import cv2
-import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import load_model
+from keras.backend import clear_session
 from tqdm.auto import tqdm
 from random import randint
 
-TEST_DIR = "../Datasets/alexandre/test"
-MODEL_NAME = "alexandre02"
+TEST_DIR = "./alexandre-real-20-GrayScale-(8, 5)-test.npy"
+MODEL_NAME = "alexandre01"
 ROW_SIZE = 8
 COL_SIZE = 5
 
 model = load_model(f"{MODEL_NAME}.h5")
+
 erros = 0
 acertos = 0
 total = 0
@@ -20,27 +22,25 @@ total = 0
 fig = plt.figure()
 num = 0
 
-for num_folder in range(10):
-    for img_name in os.listdir(os.path.join(TEST_DIR, str(num_folder))):
-        img = cv2.imread(os.path.join(TEST_DIR, str(num_folder), img_name), cv2.IMREAD_GRAYSCALE)
-        orig = img
+testing_data = np.load(TEST_DIR, allow_pickle=True)
 
-        img = cv2.resize(img, (ROW_SIZE, COL_SIZE))
-        img = img.reshape(-1, ROW_SIZE, COL_SIZE, 1)
+for img_data in testing_data:
+    img = img_data[0]
+    orig = img
+    img = img.reshape(-1, ROW_SIZE, COL_SIZE, 1)
 
-        y = fig.add_subplot(4, 5, num + 1)
+    y = fig.add_subplot(4, 5, num + 1)
+    mdl = model.predict([img])[0]
 
-        mdl = model.predict([img])[0]
+    str_label = f"Numero: {np.argmax(mdl)} ({img_data[1]})"
 
-        str_label = f"Numero: {np.argmax(mdl)} ({num_folder})"
+    print(str_label)
+    print(["{0:.2f}%".format(i * 100) for i in mdl])
 
-        print(str_label)
-        print(["{0:.2f}%".format(i * 100) for i in mdl])
-
-        y.imshow(orig, cmap='gray')
-        plt.title(str_label)
-        y.axes.get_xaxis().set_visible(False)
-        y.axes.get_yaxis().set_visible(False)
-        num += 1
+    y.imshow(orig, cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+    num += 1
 plt.show()
-os._exit(0)
+clear_session()
